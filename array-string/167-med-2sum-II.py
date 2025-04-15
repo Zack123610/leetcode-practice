63ee006e92a4d28d9789a62d57cb8066dc3216dd
Zack123610 <zack123610@gmail.com>
2025-04-16 00:41:17 +0800

    updated leetcode 167: Two sum II

---
+++ array-string/167-med-2sum-II.py
@@ -0,0 +1,20 @@
+# Leetcode 167: Two Sum II - Input Array Is Sorted
+# Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+from typing import List
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+            
+            if current_sum == target:
+                return [left + 1, right + 1]  # 1-based indexing
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
+                
+        return [-1, -1]  # This line is technically not needed as per problem constraints
+
