4060f10ccadee40a5682ccd16097025f78318ba3
Zack123610 <zack123610@gmail.com>
2025-06-28 11:14:44 +0800

    added leetcode 918: Maximum Sum Circular Subarray

--- kadane-algorithm/918-med-max-sum-circular-suarray.py
+++ kadane-algorithm/918-med-max-sum-circular-suarray.py
@@ -1,7 +1,19 @@
 # Leetcode 918: Maximum Sum Circular Subarray
 # Link: https://leetcode.com/problems/maximum-sum-circular-subarray/
 
+from typing import List
+
 class Solution:
+    # Calculate the "Minimum Subarray"
+    '''
+    Complexity Analysis
+    Here, N is the length of the input array.
+
+    Time complexity: O(N).
+    The algorithm iterates over all elements to calculate the maxSum, minSum, and sum which takes O(N) time.
+
+    Space complexity: O(1).
+    '''
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         curMax = curMin = 0
         maxSum = minSum = nums[0]
