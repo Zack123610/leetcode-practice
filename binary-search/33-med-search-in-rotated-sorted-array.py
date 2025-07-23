bafaab0e983562f0785498e984266a6b75046079
Zack123610 <zack123610@gmail.com>
2025-07-23 16:07:18 +0800

    added leetcode 33: search in rotated sorted array

--- binary-search/33-med-search-in-rotated-sorted-array.py
+++ binary-search/33-med-search-in-rotated-sorted-array.py
@@ -1,6 +1,8 @@
 # Leetcode 33: Search in Rotated Sorted Array
 # https://leetcode.com/problems/search-in-rotated-sorted-array/
 
+from typing import List
+
 class Solution:
     '''
     Approach 1: Find Pivot Index + Binary Search
