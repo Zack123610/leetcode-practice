61cf721361b8283e5173879a529e024fec832e3d
Zack123610 <zack123610@gmail.com>
2025-08-13 18:54:44 +0800

    added leetcode 108: convert sorted list to BST

---
+++ divide-and-conquer/108-easy-sorted-list-to-bst.py
@@ -0,0 +1,27 @@
+# Leetcode 108: Convert Sorted List to Binary Search Tree
+# Link: https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        def helper(l, r):
+            if l > r:
+                return None
+            
+            m = (l + r) // 2
+            root = TreeNode(nums[m])
+            root.left = helper(l, m-1)
+            root.right = helper(m+1, r)
+
+            return root
+
+        return helper(0, len(nums)-1)
