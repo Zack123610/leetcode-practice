385b62b631e2db096c3a2eb013c640c4bd4eea1a
Zack123610 <zack123610@gmail.com>
2025-08-28 10:44:03 +0800

    added leetcode 79: word search

---
+++ backtracking/79-med-word-search.py
@@ -0,0 +1,39 @@
+# Leetcode 79: Word Search
+# Link: https://leetcode.com/problems/word-search/
+
+from typing import List
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if not board or not board[0]:
+            return False
+
+        rows, cols = len(board), len(board[0])
+        visited = set()
+
+        def dfs(r: int, c: int, pos: int) -> bool:
+            # All characters matched
+            if pos == len(word):
+                return True
+
+            # Out of bounds / mismatch / already used
+            if (r < 0 or c < 0 or r >= rows or c >= cols or
+                board[r][c] != word[pos] or (r, c) in visited):
+                return False
+
+            visited.add((r, c))
+            found = (
+                dfs(r + 1, c, pos + 1) or
+                dfs(r - 1, c, pos + 1) or
+                dfs(r, c + 1, pos + 1) or
+                dfs(r, c - 1, pos + 1)
+            )
+            visited.remove((r, c))  # backtrack
+            return found
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
