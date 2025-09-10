1957b8e804affd355c6abdf4c5d6eba4f5eda784
Zack123610 <zack123610@gmail.com>
2025-09-10 13:51:57 +0800

    added leetcode 121: best time to buy and sell stock

---
+++ array-string/121-easy-best-time-to-trade-stock.py
@@ -0,0 +1,32 @@
+# Leetcode 121: Best Time to Buy and Sell Stock
+# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
+
+class Solution2:
+    '''
+    Sliding Window
+    '''
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+
+            r += 1
+            
+        return maxP
