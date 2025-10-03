28306a3b61bd1c5f545a14c6600bcfc69427cdc5
Zack123610 <zack123610@gmail.com>
2025-10-03 13:16:33 +0800

    added leetcode 242: valid anagrams

---
+++ array-string/242-easy-valid-anagrams.py
@@ -0,0 +1,20 @@
+# Leetcode 242: Valid Anagrams
+# Link: https://leetcode.com/problems/valid-anagrams/
+
+from typing import List
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        counterS, counterT = {}, {}
+        for i in range(len(s)):
+            counterS[s[i]] = 1 + counterS.get(s[i], 0)
+            counterT[t[i]] = 1 + counterT.get(t[i], 0)
+
+        for c in counterS:
+            if counterS[c] != counterT.get(c, 0):
+                return False
+        
+        return True
