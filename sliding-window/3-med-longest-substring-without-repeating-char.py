7e12cd723a0640efb01a6b85afbc894eaff679af
Zack123610 <zack123610@gmail.com>
2025-10-09 17:22:04 +0800

    updated leetcode 3

--- sliding-window/3-med-longest-substring-without-repeating-char.py
+++ sliding-window/3-med-longest-substring-without-repeating-char.py
@@ -18,3 +18,24 @@ class Solution:
 
         return res
         
+class SolutionLeetcode:
+    '''
+    Time Complexity: O(n)
+    Space Complexity: O(min(m, n))
+    '''
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        # charToNextIndex stores the index after current character
+        charToNextIndex = {}
+
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):
+            if s[j] in charToNextIndex:
+                i = max(charToNextIndex[s[j]], i)
+
+            ans = max(ans, j - i + 1)
+            charToNextIndex[s[j]] = j + 1
+
+        return ans       
