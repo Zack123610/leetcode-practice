a862f41bfe31b8273f298cf535dffd82ec2ca6db
Zack123610 <zack123610@gmail.com>
2025-10-12 11:42:06 +0800

    updated leetcode 33: search in rotatetd sorted array

--- binary-search/33-med-search-in-rotated-sorted-array.py
+++ binary-search/33-med-search-in-rotated-sorted-array.py
@@ -3,6 +3,35 @@
 
 from typing import List
 
+class Solution1:
+    '''
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+    '''
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Left half is sorted
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # Right half is sorted
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+
 class Solution:
     '''
     Approach 1: Find Pivot Index + Binary Search
