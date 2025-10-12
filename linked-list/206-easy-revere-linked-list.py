6f513e34bd73095469ae58868e2dab8ed684e983
Zack123610 <zack123610@gmail.com>
2025-10-12 19:50:23 +0800

    added leetcode 206: reverse linked list

---
+++ linked-list/206-easy-revere-linked-list.py
@@ -0,0 +1,26 @@
+# Leetcode 206: Reverse Linked List
+# Link: https://leetcode.com/problems/reverse-linked-list/
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return
+
+        cur = head
+        prev = None
+
+        while cur:
+
+            nxt = cur.next
+            cur.next = prev            
+            prev = cur
+            cur = nxt
+
+        return prev
