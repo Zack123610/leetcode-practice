98af7f3092e9fbe46b210844bce5ca1b2353bcab
Zack123610 <zack123610@gmail.com>
2025-10-25 20:26:09 +0800

    added leetcode 62: Unique Paths

---
+++ multiD-dp/62-med-unique-paths.py
@@ -0,0 +1,16 @@
+# Leetcode 62: Unique Paths
+# Link: https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    '''
+    Time Complexity: O(m*n)
+    Space Complexity: O(m*n)
+    '''
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [ [1] * n for _ in range(m) ]
+
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+
+        return dp[m-1][n-1]
