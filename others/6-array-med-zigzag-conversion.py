83971b8a448c232e8d28a9851977b645c796b383
Zack123610 <zack123610@gmail.com>
2025-11-14 15:14:00 +0800

    added leetcode 6: zigzag conversion

---
+++ others/6-array-med-zigzag-conversion.py
@@ -0,0 +1,27 @@
+# Leetcode 6: Zigzag Conversion
+# Link: https://leetcode.com/problems/zigzag-conversion/
+
+from typing import List
+
+class Solution:
+    '''
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    '''
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        res = ""
+
+        for r in range(numRows):
+            increment = (numRows - 1) * 2
+
+            for i in range(r, len(s), increment):
+                res += s[i]
+
+                if (r > 0 and r < numRows - 1 and
+                    i + increment - 2 * r < len(s)):
+                    res += s[i + increment - 2 * r]
+        
+        return res
