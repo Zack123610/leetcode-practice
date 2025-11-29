b08601c3353e05c288db88b539671e5b56dd7c56
Zack123610 <zack123610@gmail.com>
2025-11-29 14:48:20 +0800

    modified leetcode 46: permutations

--- backtracking/46-med-permutations.py
+++ backtracking/46-med-permutations.py
@@ -3,6 +3,28 @@
 
 from typing import List
 
+class SolutionLeetcode:
+    '''
+    Time Complexity: O(n * n!)
+    Space Complexity: O(n)
+    '''
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return
+            
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
+        
+        res = []
+        backtrack([])
+        return res
+
 class Solution:
     '''
     Time Complexity: O(n⋅n!)
